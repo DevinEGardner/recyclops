e2372f3adf28e622de220167bd3e9190b7b9fa09
yam3nv <yam3nv@virginia.edu>
2020-10-03 19:09:08 -0400

    Move thing

---
+++ build/precache-manifest.19d16d15325838a2a686f167eaf3c2ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602357d7cad812bce5a56266568b8cc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8a713b011c60532716f",
+    "url": "/static/css/main.6a8ce532.chunk.css"
+  },
+  {
+    "revision": "2e4fefc4fd3bcc49577b",
+    "url": "/static/js/2.6078d896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6078d896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a713b011c60532716f",
+    "url": "/static/js/main.30be2497.chunk.js"
+  },
+  {
+    "revision": "73b800585b59daa387d1",
+    "url": "/static/js/runtime-main.7ae9b67d.js"
+  }
+]);
